2d42c0f131956df0b1d7b4a540aafcbbdba187dd
Dave Turner <dave@turneris.com>
2016-02-05 22:08:59 -0800

    Change shell script to prevent the app freezing on slow Chrome launch.

--- Contents/MacOS/launch.sh
+++ Contents/MacOS/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/usr/bin/open /Applications/Google\ Chrome.app --new --args --new-window "$1" &
+/usr/bin/open /Applications/Google\ Chrome.app --new --args --new-window "$1" > /dev/null 2>&1
 sleep 1
-/usr/bin/osascript -e 'activate application "Google Chrome"' &
+/usr/bin/osascript -e 'activate application "Google Chrome"' > /dev/null 2>&1
